e404155f46e14f8c37646ae1ccab3767cfd3add0
Leo Adavirat <52106425+LeoAdavirat@users.noreply.github.com>
2024-04-23 08:20:46 +0700

    Updated week 3,4,5,6,7

--- week_5/myVisualiser.py
+++ week_5/myVisualiser.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 def cluster_and_visualise(datafilename, K, featureNames):
-    ##your code goes here
-    pass
+    data = np.genfromtxt(datafilename)
+    clusterModel = KMeans(n_clusters=2)
+    clusterModel.fit(data)
+    
 
